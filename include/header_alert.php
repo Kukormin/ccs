82e6310aee84b8ff9bfb2e6e6d019f4c3ed2f276
prod <git@cupcakestory.ru>
2016-09-27 10:57:12 +0300

    Правки с прода

--- include/header_alert.php
+++ include/header_alert.php
@@ -1,3 +1,2 @@
 <div class="header-alert" style="display:none;text-align:center;background-color: #d60059; color: white; font-family:arial, sans; padding:6px 0 6px 0">
-Дорогие друзья! <br/> 
-Желаем вам счастья и любви.</div>
+Акция! При заказе сегодня —скидка 20%</div>
