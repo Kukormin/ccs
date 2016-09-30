d1c516360cc2829f3903f16c82cea1db6dfd1ac6
Кукормин Тимофей <tim.kukom@gmail.com>
2016-09-30 18:14:37 +0400

    Изменения на странице сладких столов

--- sweet-table/index.php
+++ sweet-table/index.php
@@ -1,28 +1,25 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Сладкие столы");
-?><section class="b-topblock b-topblock--sweet-table">
-<div class="b-sweet-table-text">
-	<div class="b-sweet-table-top-text__title">
-        <? $APPLICATION->IncludeFile( '/include/sweet_table_title_inc.php', array(),
-            array(
-                'MODE'  => 'html',
-                'TEMPLATE'  => 'page_inc.php',
-            )
-        ); ?>
+?>
+	<section class="b-topblock b-topblock--pay-ship"> </section> <section class="b-bg-grey">
+	<div class="b-content-center b-reviews">
+		<div class="b-content-center--title i-padding__top-100">
+		<? $APPLICATION->IncludeFile( '/include/sweet_table_title_inc.php', array(),
+			array(
+				'MODE'  => 'html',
+				'TEMPLATE'  => 'page_inc.php',
+			)
+		); ?>
+			</div>
+	<div class="b-content-center--description">
+		<? $APPLICATION->IncludeFile( '/include/sweet_table_text_inc.php', array(),
+			array(
+				'MODE'  => 'html',
+				'TEMPLATE'  => 'page_inc.php',
+			)
+		); ?>
 	</div>
-	<div class="b-sweet-table-top-text__sub">
- <span class="b-sweet-table-top-text__sub-item">
-    <? $APPLICATION->IncludeFile( '/include/sweet_table_text_inc.php', array(),
-        array(
-            'MODE'  => 'html',
-            'TEMPLATE'  => 'page_inc.php',
-        )
-    ); ?>
- </span>
-	</div>
-</div>
- </section>
 
 <?$APPLICATION->IncludeComponent(
 	"bitrix:photogallery", 
@@ -94,4 +91,4 @@ $APPLICATION->SetTitle("Сладкие столы");
 	),
 	false
 );?>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+</section><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
