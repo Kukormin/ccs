4a2b116a7dcbf6f14fc3648b69e61ebdeb43bd4b
Кукормин Тимофей <tim.kukom@gmail.com>
2016-12-01 03:33:51 +0400

    отключил какой-то оптимизатор

--- include/body_bottom.php
+++ include/body_bottom.php
@@ -7,7 +7,9 @@ if (!defined('PRODUCTION_ZONE') || PRODUCTION_ZONE !== true)
 ?>
 <script> window._txq = window._txq || []; var s = document.createElement('script'); s.type = 'text/javascript'; s
 .async = true; s.src = '//st.targetix.net/txsp.js'; (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(s); _txq.push(['createPixel', '553f0dbe870a8e040c5d3d9b']); _txq.push(['track', 'PageView']); </script>
-
+<?
+/*
+?>
 <!-- Start Visual Website Optimizer Asynchronous Code -->
 <script type='text/javascript'>
 	var _vwo_code=(function(){
@@ -18,8 +20,9 @@ if (!defined('PRODUCTION_ZONE') || PRODUCTION_ZONE !== true)
 // DO NOT EDIT BELOW THIS LINE
 			f=false,d=document;return{use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){settings_timer=setTimeout('_vwo_code.finish()',settings_tolerance);var a=d.createElement('style'),b='body{opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;}',h=d.getElementsByTagName('head')[0];a.setAttribute('id','_vis_opt_path_hides');a.setAttribute('type','text/css');if(a.styleSheet)a.styleSheet.cssText=b;else a.appendChild(d.createTextNode(b));h.appendChild(a);this.load('//dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&r='+Math.random());return settings_timer;}};}());_vwo_settings_timer=_vwo_code.init();
 </script>
-<!-- End Visual Website Optimizer Asynchronous Code -->
+<!-- End Visual Website Optimizer Asynchronous Code --><?*/
 
+?>
 <!-- VK tracking code -->
 <script type="text/javascript">(window.Image ? (new Image()) : document.createElement('img')).src = location.protocol + '//vk.com/rtrg?r=PoDlXltXU*pSE/i0w4jJDVD5j0vJHDre2ldVzdCNsXBKxNCylQQXZ9KIk9F5l6DOsRHXfbapASH9Y4c9swoyeeeevgLc*jKfRswN10dh9JwNyLix2oB*Xsj6Y8czD1Rrpt75ponvDeKPQb7O83HOa37AAQkv5PISTbpXypfGjjc-';</script>
 <!-- end of VK tracking code -->
