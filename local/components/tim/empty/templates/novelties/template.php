addd9d2e959e77c9ac3a98b416c995e9ccbbb1f2
Кукормин Тимофей <tim.kukom@gmail.com>
2017-02-27 14:34:47 +0400

    Хиты на главную

--- local/components/tim/empty/templates/novelties/template.php
+++ local/components/tim/empty/templates/novelties/template.php
@@ -1,9 +1,14 @@
 <?
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
-$filter = array(
-	'NEW' => 'Y',
-);
+$dataNew = \Local\Catalog\Products::getDataByFilter(array('NEW' => 'Y'));
+$idsNew = $dataNew['IDS'] ? $dataNew['IDS'] : array();
+
+$dataHit = \Local\Catalog\Products::getDataByFilter(array('HIT' => 'Y'));
+$idsHit = $dataHit['IDS'] ? $dataHit['IDS'] : array();
+
+$ids = array_merge($idsNew, $idsHit);
+
 $sort = array(
 	'sort' => 'asc',
 	'created' => 'desc',
@@ -11,8 +16,7 @@ $sort = array(
 $nav = array(
 	'nTopCount' => 15
 );
-$data = \Local\Catalog\Products::getDataByFilter($filter);
-$result = \Local\Catalog\Products::get($sort, $data['IDS'], $nav);
+$result = \Local\Catalog\Products::get($sort, $ids, $nav);
 $products = $result['ITEMS'];
 
 if (!$products)
@@ -32,8 +36,21 @@ if (!$products)
 								<img src="<?= $item['PREVIEW_PICTURE'] ?>"
 								     alt="<?= $item['PIC_ALT'] ?>" title="<?= $item['PIC_TITLE'] ?>" />
 							</a>
-						</div>
-						<div class="b-mod__item-label">новинка</div>
+						</div><?
+
+						$label = '';
+						if ($item['NEW'])
+							$label = 'новинка';
+						elseif ($item['HIT'])
+							$label = 'хит';
+
+						if ($label)
+						{
+							?>
+							<div class="b-mod__item-label"><?= $label ?></div><?
+						}
+
+						?>
 					</div>
 					<div class="b-mod__item-title">
 						<a href="<?= $item['DETAIL_PAGE_URL'] ?>"><?= $item['NAME'] ?></a>
