61316e692cf347845b54d36e0680c863559431c6
Кукормин Тимофей <tim.kukom@gmail.com>
2017-04-18 14:39:07 +0400

    Убрал команду из футера

--- bitrix/templates/cupcake_inner/footer.php
+++ bitrix/templates/cupcake_inner/footer.php
@@ -41,8 +41,6 @@ if (defined('INDEX_PAGE') && INDEX_PAGE)
 			</div>
 			<div class="b-footer-info gl-block-footer-info">
 				<div class="b-footer-copy">
-					<span>Проект развивает </span><a style="color:#a3a67b;"
-					                                 href="http://komanda-a.pro/">Команда-А</a><br>
 					<span>Продвижение сайта - </span><a style="color:#a3a67b;" href="http://neyiron.ru/">Neyiron</a>
 				</div>
 				<ul class="b-footer-nav__list">
